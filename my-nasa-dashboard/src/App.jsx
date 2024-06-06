38d672316551d0cb7898d078715a91064a7f13ab
greg2105 <kinggreg2105@gmail.com>
2024-06-06 15:13:31 -0400

    sky working without buttons though.

--- my-nasa-dashboard/src/App.jsx
+++ my-nasa-dashboard/src/App.jsx
@@ -2,22 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Buttons from './components/Buttons';
 import StarrySky from './components/StarrySky';
-import TestComponent from './components/TestComponent';
-import './styles/reset.css';
+import './styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-      <div>
-      <TestComponent />
-    </div>
-      <div className="starry-sky-container">
-        <StarrySky />
-      </div>
-      {/* Rest of your app content */}
-      <Buttons />
-    </div>
-  );
-};
-
-export default App;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <Buttons />
+    <StarrySky />
+  </>
+);
