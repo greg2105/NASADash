8e9a6f44f41e96b091929a13fef6bbd48adfcdcc
greg2105 <kinggreg2105@gmail.com>
2024-06-08 19:19:17 -0400

    title and buttons are working

--- my-nasa-dashboard/src/components/Title.jsx
+++ my-nasa-dashboard/src/components/Title.jsx
@@ -1,13 +1,26 @@
 // src/Title.jsx
 import React from 'react';
 import '../styles/Title.css';
+import githubIcon from '../../public/github.svg';
+import mailIcon from '../../public/mail.svg';
 
 const Title = () => {
-    return (
-        <div className="title-container">
-          <h1 className="title">NebulAPI</h1>
-        </div>
-      );
+  return (
+    <div className="title-container">
+      <h1 className="title">NebulAPI</h1>
+      <a
+        href="https://github.com/greg2105"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="btn"
+      >
+        <img src={githubIcon} alt="GitHub" className="icon" />
+      </a>
+      <a href="mailto:gregdorazio533@gmail.com" className="btn">
+        <img src={mailIcon} alt="Email" className="icon" />
+      </a>
+    </div>
+  );
 };
 
 export default Title;
